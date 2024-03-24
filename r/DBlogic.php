b2f2622b7609e7ffe9116866d81b8328803e29fa
Shilpa3107 <shilpa.sinha3107@gmail.com>
2024-03-24 21:19:26 +0530

    Added the extra rows

--- r/DBlogic.php
+++ r/DBlogic.php
@@ -10,17 +10,17 @@ if(isset($_POST["submit"]))
     $paperTitle=$_POST["papertitle"];
     $journalName=$_POST["journalname"];
     $article=$_POST["article"];
-    //$university=$_GET["university"]; region
+    $region=$_GET["region"]; 
     $publicationDate=$_POST["publicationdate"];
     $pubYear=$_POST["pubyear"];
     $edition=$_POST["Edition"];
     $pageFrom=$_POST["from"];
     $pageTo=$_POST["to"];
     $impactFactor=$_POST["impact"];
-    //$university=$_GET["university"]; listed in scopus
-    //$university=$_GET["university"]; listed in
-    //$university=$_GET["university"]; lsited in web science
-    //$university=$_GET["university"]; peer reviewed
+    $listedInScopus=$_GET["listedInScopus"]; 
+    $listedIn=$_GET["listedIn"];
+    //$listedInWebScience=$_GET["listedInWebScience"]; 
+    //$peerReviewed=$_GET["peerReviewed"];
     $issn=$_POST["issn"];
     $isbn=$_POST["isbn"];
     $publisherName=$_POST["publishername"];
@@ -29,11 +29,11 @@ if(isset($_POST["submit"]))
     $citNumber=$_POST["citnumber"];
     $numOfCit=$_POST["noofcit"];
     $link=$_POST["link"];
-    //$university=$_GET["university"];  evidence upload
+    $upload=$_GET["upload"]; 
     $anyInfo=$_POST["anyinfo"];
     $ref=$_POST["ref"];
     $con=mysqli_connect("localhost","root","","form");
-    $sql="INSERT INTO `research`(`university`, `department`, `faculty/Scientist`, `employee_id`, `authorName`, `correspondingName`, `paperTitle`, `journalName`, `researchPaper`, `publicationDate`, `publicationYear`, `volume`, `pageFrom`, `pageInt`, `impactFactor`, `issn`, `isbn`, `publisherName`, `instituationalAffiliation`, `correspondingAuthor`, `citationIndex`, `numberOfCitation`, `link`, `anyInformation`, `ref`) VALUES ('".$university."','".$department."','".$faculty."','".$emp_id."','".$author."','".$coAuthor."','".$paperTitle."','".$journalName."','".$article."','".$article."','".$publicationDate."','".$pubYear."','".$edition."','".$pageFrom."','".$pageTo."','".$impactFactor."','".$issn."','".$isbn."','".$publisherName."','".$affiliation."','".$correspondAuthor."','".$citNumber."','".$numOfCit."','".$link."','".$anyInfo."','".$ref."')";
+    $sql="INSERT INTO `research`(`university`, `department`, `facultyScientist`, `empId`, `authorName`, `coAuthor`, `paperTitle`, `journalName`, `researchPaper`, `region`, `publicationDate`, `publicationYear`, `volume`, `pageFrom`, `pageTo`, `impactFactor`, `listedInScopus`, `listedIn`, `issn`, `isbn`, `publisherName`, `institutionalAffiliations`, `correspondingAuthor`, `citationIndex`, `citationNumber`, `link`, `evidence`, `anyInfo`, `ref`) VALUES ('".$university."','".$department."','".$faculty."','".$emp_id."','".$author."','".$coAuthor."','".$paperTitle."','".$journalName."','".$article."','".$region."','".$publicationDate."','".$pubYear."','".$edition."','".$pageFrom."','".$pageTo."','".$impactFactor."','".$listedInScopus."','".$listedIn."','".$issn."','".$isbn."','".$publisherName."','".$affiliation."','".$correspondAuthor."','".$citNumber."','".$numOfCit."','".$link."','".$upload."','".$anyInfo."','".$ref."')";
     mysqli_query($con,$sql);
     echo "Data has been inserted successfully\n";
     foreach ($_POST as $key => $value) {
